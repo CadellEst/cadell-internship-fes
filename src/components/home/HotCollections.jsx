a8ecab1d61a1518f7b93cb81543db1fdbb546758
CadellEst <139273238+CadellEst@users.noreply.github.com>
2024-09-05 18:18:58 +0100

    made 'hot collections' section dynamic. Added a dynamic carousel. Added loading state for 'hot collections' section.

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,46 +1,160 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import axios from "axios";
+import { useKeenSlider } from "keen-slider/react";
+import "keen-slider/keen-slider.min.css";
+import "../../css/carousel/Carousel.css";
 
 const HotCollections = () => {
+  const [nfts, setNfts] = useState([]);
+  let results = [];
+  let nft = {};
+  const [img, setImg] = useState(true);
+
+  useEffect(() => {
+    async function fetchNFT() {
+      setImg(false);
+      const { data } = await axios.get(
+        `https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections`
+      );
+      setNfts(data);
+      setImg(true);
+    }
+    fetchNFT();
+  }, []);
+
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [loaded, setLoaded] = useState(false);
+  const [sliderRef, instanceRef] = useKeenSlider({
+    breakpoints: {
+      "(min-width: 400px)": {
+        slides: { perView: 2, spacing: 5 },
+      },
+      "(min-width: 1000px)": {
+        slides: { perView: 4, spacing: 10 },
+      },
+    },
+    slides: { perView: 1 },
+    loop: true,
+    mode: "free-snap",
+    initial: 0,
+    slideChanged(slider) {
+      setCurrentSlide(slider.track.details.rel);
+    },
+    created() {
+      setLoaded(true);
+    },
+  });
+
   return (
-    <section id="section-collections" className="no-bottom">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-12">
-            <div className="text-center">
-              <h2>Hot Collections</h2>
-              <div className="small-border bg-color-2"></div>
-            </div>
-          </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft_coll">
-                <div className="nft_wrap">
-                  <Link to="/item-details">
-                    <img src={nftImage} className="lazy img-fluid" alt="" />
-                  </Link>
+    <>
+      <section id="section-collections" className="no-bottom">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="text-center">
+                <h2>Hot Collections</h2>
+                <div className="small-border bg-color-2"></div>
+              </div>
+              <div className="navigation-wrapper">
+                <div ref={sliderRef} className="keen-slider">
+                  <>
+                    {img
+                      ? nfts.map((nfts, index) => (
+                          <div
+                            className="keen-slider__slide col-lg-3 col-md-6 col-sm-6 col-xs-12 "
+                            key={index}
+                          >
+                            <div className="nft_coll">
+                              <div className="nft_wrap">
+                                <Link to="/item-details">
+                                  <img
+                                    src={nfts.nftImage}
+                                    className="lazy img-fluid"
+                                    alt=""
+                                  />
+                                </Link>
+                              </div>
+                              <div className="nft_coll_pp">
+                                <Link to="/author">
+                                  <img
+                                    className="lazy pp-coll"
+                                    src={nfts.authorImage}
+                                    alt=""
+                                  />
+                                </Link>
+                                <i className="fa fa-check"></i>
+                              </div>
+                              <div className="nft_coll_info">
+                                <Link to="/explore">
+                                  <h4>{nfts.title}</h4>
+                                </Link>
+                                <span>ERC-{nfts.code}</span>
+                              </div>
+                            </div>
+                          </div>
+                        ))
+                      : 
+                      new Array(4).fill(0).map((_, index) => (
+                          <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+                            <div className="nft_coll">
+                              <div className="nft_wrap__skeleton skeleton-box"></div>
+                              <div className="nft_coll_pp__skeleton skeleton-box"></div>
+                              <div className="flex-col">
+                                <div className="nft_coll_info__skeleton skeleton-box"></div>
+                                <br />
+                                <div className="nft_coll_info__skeleton2 skeleton-box"></div>
+                              </div>
+                            </div>
+                          </div>
+                        ))}
+                  </>
                 </div>
-                <div className="nft_coll_pp">
-                  <Link to="/author">
-                    <img className="lazy pp-coll" src={AuthorImage} alt="" />
-                  </Link>
-                  <i className="fa fa-check"></i>
-                </div>
-                <div className="nft_coll_info">
-                  <Link to="/explore">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <span>ERC-192</span>
-                </div>
+                {loaded && instanceRef.current && (
+                  <>
+                    <Arrow
+                      left
+                      onClick={(e) =>
+                        e.stopPropagation() || instanceRef.current?.prev()
+                      }
+                      disabled={currentSlide === 0}
+                    />
+
+                    <Arrow
+                      onClick={(e) =>
+                        e.stopPropagation() || instanceRef.current?.next()
+                      }
+                    />
+                  </>
+                )}
               </div>
             </div>
-          ))}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
+
+function Arrow(props) {
+  const disabled = props.disabled ? "arrow--disabled" : "";
+  return (
+    <svg
+      onClick={props.onClick}
+      className={`arrow ${
+        props.left ? "arrow--left" : "arrow--right"
+      } ${disabled}`}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+    >
+      {props.left && (
+        <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
+      )}
+      {!props.left && (
+        <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
+      )}
+    </svg>
+  );
+}
 
 export default HotCollections;
